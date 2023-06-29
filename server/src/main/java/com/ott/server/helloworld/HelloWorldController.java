0297cd866679ee878f00bea78510beaff6eac27f
Snu97 <snu970814@naver.com>
2023-06-29 18:14:13 +0900

    first init

---
+++ server/src/main/java/com/ott/server/helloworld/HelloWorldController.java
@@ -0,0 +1,14 @@
+package com.ott.server.helloworld;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/hello")
+public class HelloWorldController {
+    @GetMapping
+    public String helloWorld(){
+        return "HelloWorld";
+    }
+}
