2948b558eacd88aed04d173797399c78e0751c20
JeanneLee57 <jinhwa.lee.57@gmail.com>
2023-07-03 11:31:35 +0900

    feat: add background image

--- client/src/pages/Login.tsx
+++ client/src/pages/Login.tsx
@@ -1,5 +1,17 @@
+import { styled } from 'styled-components';
+import bgImg from '../assets/main_background.png';
+
 const Login = () => {
-  return <div></div>;
+  return <S_main></S_main>;
 };
 
 export default Login;
+
+const S_main = styled.main`
+  background-image: url(${bgImg});
+  background-repeat: no-repeat;
+  background-attachment: fixed;
+  background-position: center;
+  width: 100%;
+  height: 100%;
+`;
