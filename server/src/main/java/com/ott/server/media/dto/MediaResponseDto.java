bf757d5576805b09165a96b4abb02475d37fa431
백순우 <93911393+Snu97@users.noreply.github.com>
2023-07-03 11:42:38 +0900

    Update MediaResponseDto.java

--- server/src/main/java/com/ott/server/media/dto/MediaResponseDto.java
+++ server/src/main/java/com/ott/server/media/dto/MediaResponseDto.java
@@ -8,7 +8,7 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 public class MediaResponseDto {
-    private Integer id;
+    private Long id;
     private String title;
     private String mainPoster;
 }
