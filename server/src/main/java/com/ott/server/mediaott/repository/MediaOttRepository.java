df72c7482b7636c5868f4793cb6b4d27a88d7d5c
travelerjaguar <traveljaguar@gmail.com>
2023-07-04 11:59:14 +0900

    feat: mediaott entity repository 추가

---
+++ server/src/main/java/com/ott/server/mediaott/repository/MediaOttRepository.java
@@ -0,0 +1,7 @@
+package com.ott.server.mediaott.repository;
+
+import com.ott.server.mediaott.entity.MediaOtt;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MediaOttRepository extends JpaRepository<MediaOtt, Long> {
+}
