d0539d6dd268b755a8eae364ce55f88627302c0f
Jiwonp12 <ndwoo33@gmail.com>
2023-07-05 09:34:02 +0900

    feat: 푸터

--- client/src/pages/Root.tsx
+++ client/src/pages/Root.tsx
@@ -1,5 +1,6 @@
 import { Outlet } from 'react-router-dom';
 import Header from '../components/header/Header';
+import Footer from '../components/Footer';
 import { S_Root, S_Wrapper, S_Container } from '../styles/style';
 
 function Root() {
@@ -11,6 +12,7 @@ function Root() {
           <Outlet />
         </S_Container>
       </S_Wrapper>
+      <Footer />
     </S_Root>
   );
 }
