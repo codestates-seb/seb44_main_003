599bd709f838e2e39c585019b2b04187133e6710
kimdami <oka1313@gmail.com>
2023-07-07 00:10:40 +0900

    :alien: Fix: api 데이터 연결
    Fix: api 데이터 연결

--- client/src/pages/TV.tsx
+++ client/src/pages/TV.tsx
@@ -5,11 +5,36 @@ import styled from 'styled-components';
 import ListBtns from '../components/ui/ListBtns';
 
 const TV = () => {
+  const genres: string[] = [
+    'SF',
+    '스릴러',
+    '애니메이션',
+    '코미디',
+    '가족',
+    '판타지',
+    '로맨스',
+    '공포',
+    '범죄',
+    '스포츠',
+    '음악',
+    'Made in Europe',
+    'Reality TV',
+    '역사',
+    '다큐멘터리',
+    '전쟁',
+    '서부'
+  ];
+
   return (
     <S_Wrapper>
       <Banner image={image} />
       <ListBtns />
-      <SildeTV />
+      {genres.map((genre) => (
+        <>
+          <S_GenreTitle>{genre}</S_GenreTitle>
+          <SildeTV genre={genre}/>
+        </>
+      ))}
     </S_Wrapper>
   );
 };
@@ -21,4 +46,12 @@ const S_Wrapper = styled.div`
   flex-direction: column;
   justify-content: center;
   width: 100vw;
+`;
+
+const S_GenreTitle = styled.h2`
+  margin: 28px 0 5px 0;
+  padding: 0px 3.75rem;
+  color: var(--color-white-100);
+  font-size: 24px;
+  font-weight: 700;
 `;
