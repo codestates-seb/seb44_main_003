c2d54068bf4322229b1d1651aa79459eee628f3a
JeanneLee57 <jinhwa.lee.57@gmail.com>
2023-07-06 19:52:23 +0900

    fix: revert padding-top changes

--- client/src/pages/Root.tsx
+++ client/src/pages/Root.tsx
@@ -2,16 +2,12 @@ import { Outlet } from 'react-router-dom';
 import Header from '../components/header/Header';
 import Footer from '../components/Footer';
 import { S_Root, S_Wrapper, S_Container } from '../styles/style';
-import { useLocation } from 'react-router-dom';
 
 function Root() {
-  const location = useLocation();
-  const pathname = location.pathname;
-  const IsSpacedTop = !(pathname === '/tv' || pathname === '/movie');
   return (
     <S_Root>
       <Header />
-      <S_Wrapper $spacedTop={IsSpacedTop}>
+      <S_Wrapper>
         <S_Container>
           <Outlet />
         </S_Container>
