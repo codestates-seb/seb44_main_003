e43635a1cd76359b9afeecb798863db998c8ed3c
Jiwonp12 <ndwoo33@gmail.com>
2023-07-10 01:28:48 +0900

    fix: 스크롤 함수 변경

--- client/src/pages/Content.tsx
+++ client/src/pages/Content.tsx
@@ -1,7 +1,7 @@
 import { useParams } from 'react-router-dom';
 import ContentDetail from '../components/contents/ContentDetail';
 import CommentSection from '../components/comments/CommentSection';
-import { scrollToTop } from './../utils/useScrollToTop';
+import { scrollToTop } from '../utils/scrollToTop';
 
 const Content = () => {
   const { id = '' } = useParams<{ id?: string }>();
