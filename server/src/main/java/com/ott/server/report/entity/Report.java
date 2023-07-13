75e90750339d61401d0585709a1f4155f00a384d
Snu97 <snu970814@naver.com>
2023-07-13 17:16:04 +0900

    feat: report CRUD all

---
+++ server/src/main/java/com/ott/server/report/entity/Report.java
@@ -0,0 +1,36 @@
+package com.ott.server.report.entity;
+
+import com.fasterxml.jackson.annotation.JsonBackReference;
+import com.ott.server.audit.Auditable;
+import com.ott.server.media.entity.Media;
+import com.ott.server.member.entity.Member;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.checkerframework.checker.units.qual.C;
+
+import javax.persistence.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@Entity
+public class Report extends Auditable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(nullable = false)
+    private String title;
+    @Column(nullable = false)
+    private String content;
+    @Column()
+    private Boolean completion;
+    @JoinColumn(name = "MEMBER_ID")
+    @OneToOne
+    @JsonBackReference
+    private Member member;
+    @JoinColumn(name = "MEDIA_ID")
+    @ManyToOne
+    @JsonBackReference
+    private Media media;
+}
