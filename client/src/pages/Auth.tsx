9955e17ebb379e8a5eb46abfeefd21be2e3a5f73
JeanneLee57 <jinhwa.lee.57@gmail.com>
2023-07-14 14:34:30 +0900

    닉네임 글자수 제한 및 로그인 회원가입 폼 애니메이션 추가

---
+++ client/src/pages/Auth.tsx
@@ -0,0 +1,40 @@
+import { styled } from 'styled-components';
+import Authentication from '../components/authentication/FormContainer';
+
+const Auth = () => {
+  return (
+    <S_main>
+      <Authentication />
+      <img src="https://ott-main-project.s3.ap-northeast-2.amazonaws.com/main_background.webp" />
+    </S_main>
+  );
+};
+
+export default Auth;
+
+const S_main = styled.main`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 100%;
+  height: 100%;
+  > img {
+    position: absolute;
+    transform: translate(-50%, 50%) scale(3.7);
+    animation-delay: 0.1s;
+    animation-direction: normal;
+    animation-fill-mode: forwards;
+    animation: backgroundChange 1.2s ease;
+    z-index: -1;
+    @keyframes backgroundChange {
+      from {
+        filter: brightness(0);
+        transform: translate(-40%, 40%) scale(3.7);
+      }
+      to {
+        filter: brightness(100%);
+        transform: translate(-50%, 50%) scale(3.7);
+      }
+    }
+  }
+`;
