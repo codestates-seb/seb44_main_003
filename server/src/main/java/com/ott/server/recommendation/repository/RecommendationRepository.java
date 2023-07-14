f6eacc02824911d98b72f820186bd6e559fbb7fa
him0814 <him1325@gmail.com>
2023-07-14 13:04:25 +0900

    feat: 추천순으로 미디어 정렬 기능 추가

--- server/src/main/java/com/ott/server/recommendation/repository/RecommendationRepository.java
+++ server/src/main/java/com/ott/server/recommendation/repository/RecommendationRepository.java
@@ -3,7 +3,10 @@ package com.ott.server.recommendation.repository;
 import com.ott.server.media.entity.Media;
 import com.ott.server.member.entity.Member;
 import com.ott.server.recommendation.entity.Recommendation;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 
@@ -11,5 +14,8 @@ public interface RecommendationRepository extends JpaRepository<Recommendation, 
     Recommendation findByMemberAndMedia(Member member, Media media);
 
     List<Recommendation> findAllByMember(Member member);
+
+    @Query("SELECT a.media.id, COUNT(a) as count FROM Recommendation a GROUP BY a.media.id ORDER BY COUNT(a) DESC")
+    Page<Object[]> findRecommendCountByMediaIdOrderByCount(Pageable pageable);
 
 }
