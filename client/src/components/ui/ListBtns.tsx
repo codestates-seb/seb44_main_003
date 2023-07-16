d757746b76d2f855157d6ceb4a3136fb0d495bd2
Jiwonp12 <ndwoo33@gmail.com>
2023-07-16 15:36:27 +0900

    fix: 장르 버튼 css 수정

--- client/src/components/ui/ListBtns.tsx
+++ client/src/components/ui/ListBtns.tsx
@@ -3,11 +3,20 @@ import GenreBtn from './GenreBtn';
 import OttBtn from './OttBtn';
 
 function ListBtns() {
+  const genre = new URLSearchParams(location.search).get('genre');
   return (
     <S_Wrapper>
-      <OttBtn />
-      <p>|</p>
-      <GenreBtn />
+      <S_flexBox>
+        <div className="flex">
+          <OttBtn />
+        </div>
+        <S_FlexTextBox>
+          <p className="bar">OTT 검색</p>
+          <p className="bar">|</p>
+          <GenreBtn />
+        </S_FlexTextBox>
+      </S_flexBox>
+      <S_genre>{genre}</S_genre>
     </S_Wrapper>
   );
 }
@@ -17,12 +26,41 @@ export default ListBtns;
 const S_Wrapper = styled.div`
   position: relative;
   display: flex;
-  align-items: center;
+  flex-direction: column;
   padding-left: 20px;
 
-  p {
+  .bar {
     font-size: 20px;
     color: var(--color-white-80);
     margin-right: 15px;
   }
+`;
+
+const S_flexBox = styled.div`
+  display: flex;
+  align-items: center;
+  .flex {
+    display: flex;
+  }
+
+  @media only screen and (max-width: 500px) {
+    flex-direction: column;
+    align-items: flex-start;
+  }
+`;
+
+const S_FlexTextBox = styled.div`
+  display: flex;
+  align-items: center;
+
+  @media only screen and (max-width: 500px) {
+    margin-top: 10px;
+  }
+`;
+
+const S_genre = styled.h1`
+  position: absolute;
+  top: 100%;
+  padding-top: 20px;
+  color: var(--color-primary-gold);
 `;
