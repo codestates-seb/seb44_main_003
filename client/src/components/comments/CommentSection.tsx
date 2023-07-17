ad2313b547ffc92d645f6d81101e16e18938b525
JeanneLee57 <jinhwa.lee.57@gmail.com>
2023-07-17 21:03:43 +0900

    fix: 글자수 제한, 양쪽 패딩 추가

--- client/src/components/comments/CommentSection.tsx
+++ client/src/components/comments/CommentSection.tsx
@@ -1,13 +1,20 @@
+import { styled } from 'styled-components';
 import CommentForm from './CommentForm';
 import Comments from './Comments';
 
 function CommentSection() {
   return (
-    <div>
+    <S_Wrapper>
       <CommentForm />
       <Comments />
-    </div>
+    </S_Wrapper>
   );
 }
 
 export default CommentSection;
+
+const S_Wrapper = styled.section`
+  margin: 50px 0;
+  padding: 0 30px;
+  min-width: 400px;
+`;
