f5b8f1d47692b54a21ac083f704a661856c5e316
kimdami <oka1313@gmail.com>
2023-07-17 16:08:52 +0900

    :iphone: Fix: 반응형 ui 구현

--- client/src/components/banner/Banner.tsx
+++ client/src/components/banner/Banner.tsx
@@ -13,11 +13,34 @@ export default Banner;
 
 const S_ImageBox = styled.div`
   position: relative; 
+  display: flex;
+  justify-content: center;
   width: 100%;
+  height: 660px;
+
+  @media only screen and (max-width: 1200px) {
+    height: 500px;
+  }
+
+  @media only screen and (max-width: 1024px) {
+    height: 350px;
+  }
+  
+  @media only screen and (max-width: 770px) {
+    height: 260px;
+  }
+
+  @media only screen and (max-width: 480px) {
+    height: 180px;
+  }
 `;
 
 const S_BannerImage = styled.img`
   object-fit: cover;
+  width: 100%;
+  height: 100%; /*height 추가*/
+  min-width: 0; /*추가*/
+  min-height: 0; /*추가*/
 `
 
 const S_BlackLinear = styled.div`
