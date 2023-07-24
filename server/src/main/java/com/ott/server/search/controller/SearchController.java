69709a9fe79f32e15981583052f0b67a24f5f944
Snu97 <snu970814@naver.com>
2023-07-24 17:55:57 +0900

    fix/cast

--- server/src/main/java/com/ott/server/search/controller/SearchController.java
+++ server/src/main/java/com/ott/server/search/controller/SearchController.java
@@ -13,6 +13,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.io.IOException;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
