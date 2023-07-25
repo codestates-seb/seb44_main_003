8bde3bbcaa1e4dbc7f8f971d7b6394fa892a430c
travelerjaguar <traveljaguar@gmail.com>
2023-07-25 19:13:45 +0900

    Revert "fix: 처음부터만 검색되는지 테스트 필요"

--- server/src/main/java/com/ott/server/search/service/SearchService.java
+++ server/src/main/java/com/ott/server/search/service/SearchService.java
@@ -101,17 +101,11 @@ public class SearchService {
     private List<String> hibernateSearchWithTitles(String titleFragment, Pageable pageable, SearchSession searchSession) {
         List<String> titleList = new ArrayList<>();
 
-//        SearchQuery<Media> titleQuery = searchSession.search(Media.class)
-//                .where(f -> f.match()
-//                        .field("title")
-//                        .matching(titleFragment))
-//                .toQuery();
         SearchQuery<Media> titleQuery = searchSession.search(Media.class)
-                .where(f -> f.simpleQueryString()
+                .where(f -> f.match()
                         .field("title")
-                        .matching(titleFragment + "*")) // todo 와일드 카드 사용, 성능에 영향 가능, 추후에 자동완성에 특화된 suggest 사용 고려
+                        .matching(titleFragment))
                 .toQuery();
-
 
 
         List<Media> titleMediaList = titleQuery.fetchHits((int) pageable.getOffset(), pageable.getPageSize());
@@ -126,8 +120,7 @@ public class SearchService {
         List<String> castList = new ArrayList<>();
 
         SearchSourceBuilder sourceBuilder = new SearchSourceBuilder();
-//        sourceBuilder.query(QueryBuilders.matchQuery("name", titleFragment));
-        sourceBuilder.query(QueryBuilders.prefixQuery("name", titleFragment));
+        sourceBuilder.query(QueryBuilders.matchQuery("name", titleFragment));
 
         SearchRequest searchRequest = new SearchRequest("jaso");
         searchRequest.source(sourceBuilder);
